bc5f85537d01a148f1569051696fe75877203956
Oleksii Khilkevych <o.khilkevich@gastrofix.com>
2014-08-10 03:19:51 +0200

    remove unused imports

--- file-archive/file-archive-service/src/main/java/me/noroutine/ucando/ResteasyApplication.java
+++ file-archive/file-archive-service/src/main/java/me/noroutine/ucando/ResteasyApplication.java
@@ -1,6 +1,5 @@
 package me.noroutine.ucando;
 
-import javax.servlet.annotation.MultipartConfig;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
@@ -8,9 +7,6 @@ import javax.ws.rs.core.Application;
  * Created by oleksii on 07/08/14.
  */
 @ApplicationPath("/")
-//@MultipartConfig(location = "/tmp",
-//        fileSizeThreshold = 1024 * 1024 * 1024,
-//        maxFileSize = 1024 * 1024 * 1024, maxRequestSize = 1024 * 1024 * 1024)
 public class ResteasyApplication extends Application {
 
     public ResteasyApplication() {
